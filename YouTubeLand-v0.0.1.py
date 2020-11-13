f4f261a508ae347f0929e2797a323784b880e67b
chouaibcher <33702774+chouaibcher@users.noreply.github.com>
2020-11-13 17:28:39 +0100

    Update YouTubeLand-v0.0.1.py

--- YouTubeLand-v0.0.1.py
+++ YouTubeLand-v0.0.1.py
@@ -1,27 +1,37 @@
 from tkinter import *
+from pygame import *
 ###Resloution###
+
 win=Tk()
 win.geometry("400x400+450+200")
-win.configure(background="white")
+win.configure(background="#ff3030")
 win.title("YTL v0.0.1")
 win.resizable(0,0)
 
 ################
+IMPORTANT='''
+> ONLY AUDIO FORMAT
+'''
+
+
+lt8=Label(win,text=IMPORTANT,bg='red',fg='white')
+lt8.pack()
+
 lb=Label(win,text="YOUTUBE DOWNLOADER",
          font=("arial",15,"bold"),
          background="white",
-         fg="red"
+         fg="red",
          )
-lb.pack()
+lb.place(x=75,y=150)
 ###
-entry=Entry(win)
-entry.pack(ipadx=100,ipady=5)
+entry=Entry(win,width=40)
+entry.place(x=38,y=190)
 ###functions###
 def downloadtube():
     try :
         import youtube_dl
     except :
-        print("install youtube_dl before use")
+        print("install youtube_dl")
     ydl_opts = {
         'format': 'bestaudio/best',
         }
@@ -37,8 +47,31 @@ bt=Button(win,text="DOWNLOAD",command=downloadtube,
            bd=5,
            relief="raised"
           )
-bt.pack(ipadx="70")
-###
+bt.place(x=150,y=220)
+###song###################
+def play():
+    mixer.init()
+    mixer.music.load("output.ogg")
+    mixer.music.play()
+    
+play()
+
+def runson():
+    mixer.music.unpause()
+def stopson():
+    mixer.music.pause()
+
+
+bts=Button(win,text="pause",command=stopson)
+bts.place(x=340,y=280)
+
+bts1=Button(win,text="unpause",command=runson)
+bts1.place(x=325,y=310)
+
+bts1=Button(win,text="repeat",command=play)
+bts1.place(x=340,y=340)
+
+#######################3
 lbc=Label(win,text="FOLLOW ME ON INSTAGRAM @chouaibcher7",
           background="white",
           foreground="black",
@@ -50,5 +83,3 @@ lbc.pack(side="bottom")
 win.mainloop()
 
 #Devlopped by @chouaibcher7
-
-#YouTubeLand-v0.0.1
